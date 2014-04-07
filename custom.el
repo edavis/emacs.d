f9338233b9b8d4847ea4028e074c13df3828b3fe
Eric Davis <eric@davising.com>
2014-04-06 21:19:05 -0700

    Darken Org mode PROPERTY keys and values

--- custom.el
+++ custom.el
@@ -28,4 +28,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-priority ((t (:inherit font-lock-keyword-face))))
+ '(org-property-value ((t (:foreground "grey"))) t)
+ '(org-special-keyword ((t (:inherit font-lock-keyword-face :foreground "darkslategrey")))))
